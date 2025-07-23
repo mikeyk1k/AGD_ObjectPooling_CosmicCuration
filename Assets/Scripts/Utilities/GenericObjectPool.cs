43f574c71be50b9e32cb092d5aba4d632ac7ba88
Mikey <khangnd@rocketstudio.com.vn>
2025-07-23 11:31:04 +0700

    update BulletPool and EnemyPool

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+        protected T GetItem()
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(i => !i.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    return item.Item;
+                }
+            }
+            return CreateNewPooledItem();
+        }
+        private T CreateNewPooledItem()
+        {
+            PooledItem<T> newItem = new PooledItem<T>();
+            newItem.Item = CreateItem();
+            newItem.isUsed = true;
+            pooledItems.Add(newItem);
+            return newItem.Item;
+        }
+        protected virtual T CreateItem()
+        {
+            throw new System.NotImplementedException("Child Class dont have implementation CreateItem() ");
+        }
+
+        public void ReturnItem(T item)
+        {
+            PooledItem<T> pooledItem = pooledItems.Find(i => i.Item.Equals(item));
+            if (pooledItem != null)
+            {
+                pooledItem.isUsed = false;
+            }
+        }
+
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool isUsed;
+        }
+    }
+}
