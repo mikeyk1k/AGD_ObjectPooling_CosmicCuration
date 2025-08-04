3f225ce3b55fc2554db9b41ecda26aaa5b4bcab8
Mikey <khangnd@rocketstudio.com.vn>
2025-08-04 10:07:33 +0700

    Create VFXPool

---
+++ Assets/Scripts/VFX/VFXPool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using CosmicCuration.Utilities;
+using System.Collections.Generic;
+
+namespace CosmicCuration.VFX
+{
+    public class VFXPool : GenericObjectPool<VFXController>
+    {
+        private VFXView prefabToSpawn;
+
+        public VFXController GetVFX(VFXView vfxView)
+        {
+            this.prefabToSpawn = vfxView;
+            return GetItem<VFXController>();
+        }
+        protected override VFXController CreateItem<T>()
+        {
+
+            if (typeof(T) == typeof(VFXController))
+            {
+                VFXController vfxToPlay = new VFXController(prefabToSpawn);
+                return vfxToPlay;
+            }
+            throw new System.Exception($"VFX Type not supported");
+        }
+    }
+}
